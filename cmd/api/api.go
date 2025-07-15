b213f0f4eb7448f05048f966076b28bdadeddcc3
sawmeraw <pandeysameer96@gmail.com>
2025-07-15 22:53:43 +0930

    created repository interfaces and models

---
+++ cmd/api/api.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sawmeraw/gogo/internal/store"
+)
+
+type application struct {
+	config config
+	store  store.Storage
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
+func (app *application) mount() http.Handler {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	router.Use(middleware.Timeout(60 * time.Second))
+
+	router.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+
+	})
+	return router
+}
+
+func (app *application) run(mux http.Handler) error {
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server has started at port %s", app.config.addr)
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sawmeraw/gogo
+
+go 1.24.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)

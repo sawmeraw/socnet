d2d8e459a372b6977e0d938fe7a4732d6d78147c
sawmeraw <pandeysameer96@gmail.com>
2025-07-23 20:17:57 +0930

    context middleware added

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"strconv"
@@ -49,7 +50,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func (app *application) GetPostHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 
 	idParam := chi.URLParam(r, "postID")
 	id, err := strconv.ParseInt(idParam, 10, 64)
@@ -83,4 +84,57 @@ func (app *application) GetPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+func (app *application) deletePostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+	if err := app.store.Posts.DeleteByID(ctx, id); err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			app.notFoundResponse(w, r, err)
+		default:
+			app.statusInternalServerError(w, r, err)
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) postsContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "postID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.statusInternalServerError(w, r, err)
+			return
+		}
+		ctx := r.Context()
+
+		post, err := app.store.Posts.GetByID(ctx, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.notFoundResponse(w, r, err)
+			default:
+				app.statusInternalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx = context.WithValue(ctx, "post", post)
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
 }
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -76,3 +76,23 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 	return &post, nil
 
 }
+
+func (s *PostStore) DeleteByID(ctx context.Context, id int64) error {
+	query := `DELETE FROM posts WHERE id = $1`
+	res, err := s.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+
+}

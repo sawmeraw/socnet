a74e47368757eb21416518c62eeb26756f4ac82a
sawmeraw <pandeysameer96@gmail.com>
2025-07-26 23:56:52 +0930

    user handler added

--- internal/store/users.go
+++ internal/store/users.go
@@ -17,6 +17,30 @@ type UserStore struct {
 	db *sql.DB
 }
 
+func (s *UserStore) GetByID(ctx context.Context, id int64) (*User, error) {
+
+	query := `
+		SELECT id, username, email, password, created_at FROM users
+		WHERE id = $1
+	`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	user := &User{}
+
+	if err := s.db.QueryRowContext(ctx, query).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return user, nil
+
+}
+
 func (s *UserStore) Create(ctx context.Context, user *User) error {
 	query := `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id, created_at`
 
